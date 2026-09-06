f191ace97ce85b7e45fe389123507eb43d2457a3
guelliliraiane-a11y <guellili.raiane@gmail.com>
2026-09-06 18:15:20 +0200

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
+      <h1>Grand livre énergétique</h1>
+      <p>L'application est en ligne et fonctionnelle !</p>
+    </div>
+  )
+}
