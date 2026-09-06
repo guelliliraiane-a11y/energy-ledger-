5be40b885ceb96c14dd636818fd501801ee74a2f
guelliliraiane-a11y <guellili.raiane@gmail.com>
2026-09-06 20:05:21 +0200

    main.jsx

--- main.jsx
+++ main.jsx
@@ -1,9 +1,4 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+document.getElementById('root').innerHTML = `
+  <h1>MAIN.JSX FONCTIONNE</h1>
+  <p>Le fichier main.jsx est bien chargé.</p>
+`
