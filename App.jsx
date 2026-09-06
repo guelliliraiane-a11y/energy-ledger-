14ac5dfeddee4e95c9e3ca6cb30d53eefd00f751
guelliliraiane-a11y <guellili.raiane@gmail.com>
2026-09-06 18:28:06 +0200

    App.jsx

--- App.jsx
+++ App.jsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
       <h1>Grand livre énergétique</h1>
-      <p>L'application est en ligne et fonctionnelle !</p>
+      <p>L'application est en ligne !</p>
     </div>
   )
 }
